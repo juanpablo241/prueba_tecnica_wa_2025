e378af1e30e40297e4e0353b56786665465c0936
juanpablo241 <simonrodas241@gmail.com>
2025-05-04 11:01:44 -0500

    env

--- actions/projection/page.ts
+++ actions/projection/page.ts
@@ -2,9 +2,7 @@
 
 export const getProyeccion = async () => {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-  console.log("Base URL:", baseUrl); // Log the base URL for debugging
-  console.log("API URL:", process.env.NEXT_PUBLIC_API_URL); // Log the API URL for debugging
-  console.log(`${baseUrl}/api/projection`); // Log the full URL for debugging
+
   const response = await fetch(`${baseUrl}/api/projection`, {
     method: "GET",
     headers: {
